4fdb25168c15c8de48306de432c4187295c93d4f
wiggooo <wigert.joakim@outlook.com>
2025-11-27 21:19:53 +0100

    added and finished Singleton enum setup

--- src/assignment3_singleton/Enum/MainEnum.java
+++ src/assignment3_singleton/Enum/MainEnum.java
@@ -1,4 +1,9 @@
 package assignment3_singleton.Enum;
 
+import assignment3_singleton.RootUser;
+
 public class MainEnum {
+    public static void main(String[] args) {
+        System.out.println(RootUserEnum.INSTANCE);
+    }
 }
---
+++ src/assignment3_singleton/Enum/RootUserEnum.java
@@ -0,0 +1,23 @@
+package assignment3_singleton.Enum;
+
+public enum RootUserEnum{
+
+    INSTANCE;
+
+    private int UID = 0;
+    private int GID = 0;
+    private String userName = "root";
+    private String name = "Super user";
+    private String home = "/root";
+    private String shell = "/bin/sh";
+
+    @Override
+    public String toString(){
+        return new StringBuilder(userName)
+                .append("(").append(name).append(")")
+                .append(" ").append(UID).append(":").append(GID)
+                .append(" home: ").append(home).append(" ")
+                .append("shell: ").append(shell)
+                .toString();
+    }
+}
